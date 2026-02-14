772929e3babda2305889a26f77495448e098e16a
João Schulz <65256627+aqueleschulz@users.noreply.github.com>
2026-02-14 15:05:55 -0300

    chore: initial project structure with Spring Boot 3 and Java 21
    Initializes project using Spring Initializr with Web, JPA, H2, Lombok, and Validation dependencies.

---
+++ src/main/java/com/github/aqueleschulz/pandora_vault/PandoraVaultApplication.java
@@ -0,0 +1,13 @@
+package com.github.aqueleschulz.pandora_vault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PandoraVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PandoraVaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/aqueleschulz/pandora_vault/PandoraVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.aqueleschulz.pandora_vault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PandoraVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
